02b787a3d17ec585b77a20c88e6983822c7fe71a
Gouthami Hasthapuram <Gouthami.Hasthapuram@moxiesystems.com>
2019-06-26 16:32:21 +0530

    Added Dirctory

--- Dummy/src/Print.java
+++ Dummy/src/Print.java
@@ -3,7 +3,8 @@ public class Print {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-     System.out.println("Hello world");
+		for(int i=0;i<10;i++)
+     System.out.println("Hello world "+(i+i));
 	}
 
 }
